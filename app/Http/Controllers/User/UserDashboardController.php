bd7be23333eb84b21cf8fe10e88600aed2bfe70b
arpit <arpitp@webmobtech.com>
2026-01-20 13:39:45 +0530

    updated admin and usercontroller also added role middleware

--- app/Http/Controllers/User/UserDashboardController.php
+++ app/Http/Controllers/User/UserDashboardController.php
@@ -8,4 +8,9 @@ use Illuminate\Http\Request;
 class UserDashboardController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        // Temporary response until Blade is added
+        return response('User Dashboard', 200);
+    }
 }
